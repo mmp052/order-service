df2edcfb4bf4a54171bf40d55d4d4997c2b6344b
mmp052 <mateusmoreirammp052@gmail.com>
2025-06-04 02:00:56 -0300

    agora vai

--- src/main/resources/db/migration/V2025.02.21.002__create_table_account.sql
+++ src/main/resources/db/migration/V2025.02.21.002__create_table_account.sql
@@ -5,12 +5,12 @@ CREATE TABLE orders.orders (
     id_account    VARCHAR(36)    NOT NULL
 );
 
-CREATE TABLE orders.items (
-    id_item    VARCHAR(36)    NOT NULL PRIMARY KEY,
-    id_order   VARCHAR(36)    NOT NULL,
-    id_product VARCHAR(36)    NOT NULL,
-    quantity   INT            NOT NULL,
-    item_price DECIMAL(10,2)  NOT NULL,
-    CONSTRAINT fk_item_order FOREIGN KEY (id_order)
+CREATE TABLE orders.order_items (
+    id_order_item VARCHAR(36)    NOT NULL PRIMARY KEY,
+    id_order      VARCHAR(36)    NOT NULL,
+    id_product    VARCHAR(36)    NOT NULL,
+    quantity      INT            NOT NULL,
+    total         DECIMAL(10,2)  NOT NULL,
+    CONSTRAINT fk_order_item_order FOREIGN KEY (id_order)
         REFERENCES orders.orders(id_order)
 );
